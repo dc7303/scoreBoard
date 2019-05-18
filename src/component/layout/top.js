fdedcc0c4716b5ee0fccf34cb1669677961afdd4
Mark42 <dc7303@gmail.com>
2019-05-18 11:29:59 +0900

    타이머기능 구현

--- src/component/layout/top.js
+++ src/component/layout/top.js
@@ -6,14 +6,62 @@ export class Top extends Component {
 
   state = {
     min: '00',
-    sec: '00'
-  }
+    sec: '00',
+    interval: 0,
+    isStart: false,
+  };
 
   constructor(props) {
     super(props);
     this.minSelectHandler = this.minSelectHandler.bind(this);
     this.secSelectHandler = this.secSelectHandler.bind(this);
     this.resetHandler = this.resetHandler.bind(this);
+    this.startHandler = this.startHandler.bind(this);
+    this.getDateValue = this.getDateValue.bind(this);
+    this.timerCallback = this.timerCallback.bind(this);
+  }
+
+  getDateValue() {
+    const minMs = this.state.min * 60000;       // 분
+    const secMs = this.state.sec * 1000;        // 초
+
+    return new Date((minMs + secMs) - 1000);
+  }
+
+  startHandler() {
+    if (this.state.isStart) {
+      return;
+    }
+
+    if (this.state.min === '00' && this.state.sec === '00') {
+      alert('시간을 셋팅해주세요.');
+      return;
+    }
+    
+    const interval = setInterval(this.timerCallback, 1000);
+    this.setState({
+      interval,
+      isStart: true
+    });
+  }
+
+  timerCallback() {
+    const date = this.getDateValue();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    if (min === 0 && sec === 0) {
+      clearInterval(this.state.interval);
+      this.initSelect();
+      this.setState({
+        isStart: false
+      });
+    }
+
+    this.setState({
+      min: this.formatValue(min),
+      sec: this.formatValue(sec),
+    });
   }
 
   minSelectHandler(event) {
@@ -33,6 +81,13 @@ export class Top extends Component {
       min: '00',
       sec: '00'
     });
+    
+    if (this.state.isStart) {
+      clearInterval(this.state.interval);
+      this.setState({
+        isStart: false
+      });
+    }
 
     this.initSelect();
   }
@@ -40,6 +95,15 @@ export class Top extends Component {
   initSelect() {
     document.getElementById('minSelect').selectedIndex = 0;
     document.getElementById('secSelect').selectedIndex = 0;
+  }
+
+  
+  formatValue(value) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+
+    return value;
   }
 
   render() {
@@ -71,7 +135,7 @@ export class Top extends Component {
       </div>
       <div className="timer">{this.state.min}:{this.state.sec}</div>
       <div className="control-box">
-        <button>start</button>
+        <button onClick={this.startHandler}>start</button>
         <button onClick={this.resetHandler}>reset</button>
       </div>
     </div>
